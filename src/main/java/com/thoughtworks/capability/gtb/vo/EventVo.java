f24b7d80907c845da1b398372d481ece6adf2232
yichi zhang <yichi.zhang@thoughtworks.com>
2020-06-12 11:38:55 +0800

    [zycsustc]-can get events info correctly

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,16 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import java.io.IOException;
+import java.sql.Time;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +27,23 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+
+  @JsonSerialize(using = TimeSerializer.class)
   private LocalDateTime time;
+
+  @JsonUnwrapped
   private UserVo user;
+
+  private static class TimeSerializer extends StdSerializer<LocalDateTime> {
+
+    protected TimeSerializer() {
+      super(LocalDateTime.class);
+    }
+
+    @Override
+    public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+      gen.writeNumber(value.atZone(ZoneOffset.UTC).toEpochSecond());
+    }
+  }
+
 }
--- src/main/java/com/thoughtworks/capability/gtb/vo/UserVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/UserVo.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserVo {
 
+  @JsonProperty("userId")
   private String id;
+  @JsonProperty("userName")
   private String name;
+
 }
